e7074c9ad57993a1d08c6d994359a48d7eb8651a
Lucas Cirille <lucas.cirille@gmail.com>
2026-08-06 18:20:50 -0300

    Auto-commit: 2026-08-06 18:20:50

--- modules/git-deploy-key.nix
+++ modules/git-deploy-key.nix
@@ -1,16 +1,16 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
-  username = "neo"; # 👈 Using your user from default.nix
+  # Toma el primer usuario normal definido en el sistema dinámicamente
+  normalUsers = lib.filterAttrs (_: u: u.isNormalUser) config.users.users;
+  username = builtins.head (builtins.attrNames normalUsers);
 in
 {
-  # 1. Point sops-nix to your encrypted secrets file and host key
   sops = {
     defaultSopsFile = ../secrets/secrets.yaml;
     age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   };
 
-  # 2. Tell sops-nix where to place the decrypted deploy key
   sops.secrets.git_deploy_key = {
     path = "/home/${username}/.ssh/github-myproject-deploy";
     owner = username;
@@ -18,7 +18,6 @@ in
     mode = "0600";
   };
 
-  # 3. Configure SSH host alias
   programs.ssh.extraConfig = ''
     Host github-myproject
       HostName github.com
