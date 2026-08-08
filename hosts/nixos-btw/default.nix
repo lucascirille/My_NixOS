a8efc89185a88684bcf96c8c75c4a2cf519eaa3f
Lucas Cirille <lucas.cirille@gmail.com>
2026-08-08 02:11:14 -0300

    Auto-commit: 2026-08-08 02:11:14

--- hosts/nixos-btw/default.nix
+++ hosts/nixos-btw/default.nix
@@ -121,13 +121,15 @@
 
   security.pam.services.i3lock = {};
 
-  # programs.i3lock.enable = true;
+  programs.i3lock = {
+  enable = true;
+  package = pkgs.i3lock-color;
+  };
 
   # System Packages & Fonts
   environment.systemPackages = with pkgs; [
     brightnessctl
     xarchiver
-    i3lock-color
   ];
 
   fonts.packages = with pkgs; [
