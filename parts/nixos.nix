facb50a10a31615c523e119ce0bdff8d47a98b8c
Lucas Cirille <lucas.cirille@gmail.com>
2026-08-18 18:43:32 -0300

    Auto-commit: 2026-08-18 18:43:32

--- parts/nixos.nix
+++ parts/nixos.nix
@@ -7,14 +7,14 @@ in
   flake.nixosConfigurations.nixos-btw = inputs.nixpkgs.lib.nixosSystem {
     specialArgs = { inherit inputs username; };
     modules = [
-      ../hosts/nixos-btw/default.nix
+      ../hosts/nixos-btw/nixos-btw.nix
       inputs.home-manager.nixosModules.home-manager
       inputs.sops-nix.nixosModules.sops
       {
         home-manager = {
           useGlobalPkgs = true;
           useUserPackages = true;
-          users.${username} = import ../home/default.nix;
+          users.${username} = import ../home/home.nix;
           backupFileExtension = "backup";
           extraSpecialArgs = { inherit inputs username; };
         };
