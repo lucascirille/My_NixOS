c19b3677a768cab9b2cadb453cc90871d6994855
Lucas Cirille <lucas.cirille@gmail.com>
2026-08-19 20:43:52 -0300

    Auto-commit: 2026-08-19 20:43:52

--- specialisations/vm.nix
+++ specialisations/vm.nix
@@ -1,5 +1,14 @@
 { ... }:
 
 {
+# 1. Enable Hyper-V guest integration daemons
   virtualisation.hypervGuest.enable = true;
+
+  # 2. Enable XRDP with Qtile and audio redirection
+  services.xrdp = {
+    enable = true;
+    defaultWindowManager = "qtile start";
+    openFirewall = true;
+    audio.enable = true; # Compiles and enables xrdp-pulseaudio/pipewire sinks
+  };
 }
