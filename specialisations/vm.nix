f17c1c955b0b1a790b9787c38472160ec73cfe1b
Lucas Cirille <lucas.cirille@gmail.com>
2026-08-19 21:11:22 -0300

    Auto-commit: 2026-08-19 21:11:22

--- specialisations/vm.nix
+++ specialisations/vm.nix
@@ -2,16 +2,9 @@
 
 {
 # --- KVM / QEMU Support ---
-  services.spice-vdagentd.enable = true; # Dynamic resize & clipboard in virt-manager
-  services.qemuGuest.enable = true;      # Guest integration for KVM/QEMU
+  # services.spice-vdagentd.enable = true; # Dynamic resize & clipboard in virt-manager
+  # services.qemuGuest.enable = true;      # Guest integration for KVM/QEMU
 
   # --- Hyper-V / Universal XRDP Support ---
   virtualisation.hypervGuest.enable = true;
-
-  services.xrdp = {
-    enable = true;
-    defaultWindowManager = "qtile start";
-    openFirewall = true;
-    audio.enable = true; # Audio redirection
-  };
 }
