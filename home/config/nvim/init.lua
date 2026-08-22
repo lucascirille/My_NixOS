f15e0d5e76cc2333be1e0ebf7cb3a8a07696d1d3
Lucas Cirille <lucas.cirille@gmail.com>
2026-08-21 21:40:09 -0300

    Auto-commit: 2026-08-21 21:40:09

--- home/config/nvim/init.lua
+++ home/config/nvim/init.lua
@@ -1,5 +1,6 @@
 vim.opt.clipboard = "unnamedplus"
 
+-- 1. Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -15,3 +16,47 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
+
+-- 2. Setup Plugins
+require("lazy").setup({
+  -- Theme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
+
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "nix", "bash", "markdown", "markdown_inline", "c", "vim", "vimdoc" },
+        highlight = { enable = true },
+      })
+    end,
+  },
+
+  -- LSP & Completion
+  { "neovim/nvim-lspconfig" },
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-path" },
+  { "L3MON4D3/LuaSnip" },
+  { "saadparwaiz1/cmp_luasnip" },
+
+  -- Utilities & UI
+  { "nvim-lua/plenary.nvim" },
+  { "nvim-tree/nvim-web-devicons" },
+  { "zbirenbaum/copilot.lua", event = "InsertEnter", opts = {} },
+  { "tpope/vim-fugitive" },
+  { "michaelrommel/nvim-silicon", opts = {} },
+  { "epwalsh/obsidian.nvim", version = "*", lazy = true, ft = "markdown" },
+  { "stevearc/oil.nvim", opts = {} },
+  { "folke/snacks.nvim", priority = 1000, lazy = false, opts = {} },
+  { "lervag/vimtex", ft = "tex" },
+})
