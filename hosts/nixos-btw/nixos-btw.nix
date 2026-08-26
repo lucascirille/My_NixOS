442c4205077c3bb074a193c0492baf53a58c81c2
Lucas Cirille <lucas.cirille@gmail.com>
2026-08-26 14:35:52 -0300

    Auto-commit: 2026-08-26 14:35:52

--- hosts/nixos-btw/nixos-btw.nix
+++ hosts/nixos-btw/nixos-btw.nix
@@ -110,6 +110,18 @@ programs.firejail = {
         executable = "${lib.getBin pkgs.brave}/bin/brave";
         profile = "${pkgs.firejail}/etc/firejail/brave.profile";
       };
+          librewolf = {
+      executable = "${pkgs.librewolf}/bin/librewolf";
+      profile = "${pkgs.firejail}/etc/firejail/librewolf.profile";
+      extraArgs = [
+        # Required for U2F USB stick
+        "--ignore=private-dev"
+        # Enforce dark mode
+        "--env=GTK_THEME=Adwaita:dark"
+        # Enable system notifications
+        "--dbus-user.talk=org.freedesktop.Notifications"
+      ];
+    };
     };
   };
   
