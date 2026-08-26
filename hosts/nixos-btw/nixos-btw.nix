7456742af48d74bc3e970f68edc501cfd3589114
Lucas Cirille <lucas.cirille@gmail.com>
2026-08-26 14:14:40 -0300

    Auto-commit: 2026-08-26 14:14:40

--- hosts/nixos-btw/nixos-btw.nix
+++ hosts/nixos-btw/nixos-btw.nix
@@ -107,10 +107,8 @@ programs.firejail = {
     enable = true;
     wrappedBinaries = {
       brave = {
-        executable = "${pkgs.brave}/bin/brave";
-        extraArgs = [
-          "--private-home=.config/BraveSoftware"
-        ];
+        executable = "${lib.getBin pkgs.brave}/bin/brave";
+        profile = "${pkgs.firejail}/etc/firejail/brave.profile";
       };
     };
   };
