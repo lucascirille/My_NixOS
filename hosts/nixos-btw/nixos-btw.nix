28dce3fcc87ecc1d335d9dd3f1668b799b30d097
Lucas Cirille <lucas.cirille@gmail.com>
2026-08-27 17:18:05 -0300

    Auto-commit: 2026-08-27 17:18:05

--- hosts/nixos-btw/nixos-btw.nix
+++ hosts/nixos-btw/nixos-btw.nix
@@ -290,7 +290,6 @@ hardware.graphics = {
   # System Packages & Fonts
   environment.systemPackages = with pkgs; [
 
-  polkit_gnome # gui for polkit
 
 
 
@@ -337,20 +336,19 @@ hardware.graphics = {
 
 
 
-  systemd.user.services.polkit-gnome-authentication-agent-1 = {
-    description = "polkit-gnome-authentication-agent-1";
-    wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    
-    serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-    };
+systemd.user.services.polkit-kde-authentication-agent-1 = {
+  description = "polkit-kde-authentication-agent-1";
+  wantedBy = [ "graphical-session.target" ];
+  wants = [ "graphical-session.target" ];
+  after = [ "graphical-session.target" ];
+  serviceConfig = {
+    Type = "simple";
+    ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
+    Restart = "on-failure";
+    RestartSec = 1;
+    TimeoutStopSec = 10;
   };
+};
 
 # ==========================================
   # CONFIGURACIÓN EN EL HOST (FUERA DEL CONTENEDOR)
