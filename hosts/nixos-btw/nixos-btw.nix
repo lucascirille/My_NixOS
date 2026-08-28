7a7c6048c3f7899d625c41feada0164e798b6f88
Lucas Cirille <lucas.cirille@gmail.com>
2026-08-27 21:54:58 -0300

    Auto-commit: 2026-08-27 21:54:58

--- hosts/nixos-btw/nixos-btw.nix
+++ hosts/nixos-btw/nixos-btw.nix
@@ -47,8 +47,8 @@ stylix = {
 
   fonts = {
     monospace = {
-      package = pkgs.jetbrains-mono;
-      name = "JetBrains Mono";
+      package = pkgs.nerd-fonts.jetbrains-mono;
+      name = "JetBrainsMono Nerd Font";
     };
     sansSerif = {
       package = pkgs.inter;
