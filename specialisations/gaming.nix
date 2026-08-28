84adb7c1e70ccfcd6ee42e5384889d1db63186de
Lucas Cirille <lucas.cirille@gmail.com>
2026-08-28 12:13:49 -0300

    Auto-commit: 2026-08-28 12:13:49

--- specialisations/gaming.nix
+++ specialisations/gaming.nix
@@ -5,7 +5,7 @@
   # Inyectamos los drivers y permisos físicos solo cuando inicias en este modo
   # services.xserver.videoDrivers = [ "amdgpu" ];
   # String generico para potenciar placas AMD, verificar si funciona para su modelo
-  boot.kernelParams = [ "amdgpu.ppfeaturemask=0xffffffff" ];
+  # boot.kernelParams = [ "amdgpu.ppfeaturemask=0xffffffff" ];
 
 
   hardware.graphics = {
