1a2abb448fad02274f594b7fba10a10f4533b397
Lucas Cirille <lucas.cirille@gmail.com>
2026-08-28 19:41:21 -0300

    Auto-commit: 2026-08-28 19:41:21

--- specialisations/gaming.nix
+++ specialisations/gaming.nix
@@ -1,36 +1,35 @@
 { config, pkgs, lib, ... }:
 
 {
-  # ---- SOLO PARA AMD ------
-  hardware.amdgpu.overdrive.enable = true;
-  # String generico para potenciar placas AMD, verificar si funciona para su modelo
+  # string generico para potenciar placas amd, verificar si funciona para su modelo
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  
   boot.kernelParams = [ "amdgpu.ppfeaturemask=0xffffffff" ];
 
   hardware.graphics = {
       enable = true;
       enable32Bit = true;
       extraPackages = with pkgs; [
-        intel-media-driver # aceleracion por hardware Intel
+        intel-media-driver # aceleracion por hardware intel
       ];
     };
 
-  # Herramientas exclusivas para jugar
+  # herramientas exclusivas para jugar
   environment.systemPackages = with pkgs; [
     mangohud
     mangojuice
     lact
   ];
 
-  # Habilita el servicio de LACT correctamente de forma nativa
+  # habilita el servicio de lact correctamente de forma nativa
   services.lact.enable = true;
 
-# FIX: Inject systemd (for busctl) and system wrappers (for sudo)
-  # so LACT can run `sudo -u user busctl` to reach GameMode DBus.
+  # fix: inject systemd (for busctl) and system wrappers (for sudo)
+  # so lact can run `sudo -u user busctl` to reach gamemode dbus.
   systemd.services.lactd.path = with pkgs; [ 
     systemd 
     gamemode 
     bash
     "/run/wrappers"
   ];
-
 }
