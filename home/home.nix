23abed50283e86af03f0e925b1d35df163198319
Lucas Cirille <lucas.cirille@gmail.com>
2026-08-27 23:15:24 -0300

    Auto-commit: 2026-08-27 23:15:24

--- home/home.nix
+++ home/home.nix
@@ -346,22 +346,22 @@ in
     plugins = with pkgs.tmuxPlugins; [
       sensible
       yank
-      {
-        plugin = catppuccin;
-        extraConfig = ''
-          set -g @catppuccin_flavor "mocha"
-          set -g @catppuccin_window_status_style "rounded"
-          set -g @catppuccin_window_number_position "right"
-          set -g @catppuccin_window_default_fill "number"
-          set -g @catppuccin_window_default_text "#W"
-          set -g @catppuccin_window_current_fill "number"
-          set -g @catppuccin_window_current_text "#W"
-          set -g @catppuccin_status_modules_right "directory user host session"
-          set -g @catppuccin_status_fill "icon"
-          set -g @catppuccin_status_connect_separator "no"
-          set -g @catppuccin_directory_text "#{pane_current_path}"
-        '';
-      }
+      # {
+      #   plugin = catppuccin;
+      #   extraConfig = ''
+      #     set -g @catppuccin_flavor "mocha"
+      #     set -g @catppuccin_window_status_style "rounded"
+      #     set -g @catppuccin_window_number_position "right"
+      #     set -g @catppuccin_window_default_fill "number"
+      #     set -g @catppuccin_window_default_text "#W"
+      #     set -g @catppuccin_window_current_fill "number"
+      #     set -g @catppuccin_window_current_text "#W"
+      #     set -g @catppuccin_status_modules_right "directory user host session"
+      #     set -g @catppuccin_status_fill "icon"
+      #     set -g @catppuccin_status_connect_separator "no"
+      #     set -g @catppuccin_directory_text "#{pane_current_path}"
+      #   '';
+      # }
     ];
   };
 
