442384103e72140053f0aa2ed2edb56ccd98a517
Lucas Cirille <lucas.cirille@gmail.com>
2026-09-01 20:17:00 -0300

    Auto-commit: 2026-09-01 20:17:00

--- home/home.nix
+++ home/home.nix
@@ -592,20 +592,20 @@ programs.tmux = {
     </actions>
   '';
 
-# systemd.user.services.keepassxc = {
-#   Unit = {
-#     Description = "KeePassXC password manager daemon";
-#     After = [ "graphical-session.target" ];
-#   };
-#   Service = {
-#     # Keep the sleep just in case your compositor takes a moment to settle
-#     ExecStartPre = "${pkgs.coreutils}/bin/sleep 2";
-#     ExecStart = "${pkgs.keepassxc}/bin/keepassxc --minimized";
-#     Restart = "on-failure";
-#   };
-#   Install = {
-#     WantedBy = [ "graphical-session.target" ];
-#   };
-# };
+systemd.user.services.keepassxc = {
+  Unit = {
+    Description = "KeePassXC password manager daemon";
+    After = [ "graphical-session.target" ];
+  };
+  Service = {
+    # Keep the sleep just in case your compositor takes a moment to settle
+    ExecStartPre = "${pkgs.coreutils}/bin/sleep 2";
+    ExecStart = "${pkgs.keepassxc}/bin/keepassxc --minimized";
+    Restart = "on-failure";
+  };
+  Install = {
+    WantedBy = [ "graphical-session.target" ];
+  };
+};
 
 }
