5460a81bcbe3bd715b1f3d55fbda25b429e596b8
Lucas Cirille <lucas.cirille@gmail.com>
2026-09-07 16:20:29 -0300

    Auto-commit: 2026-09-07 16:20:29

--- parts/nixos.nix
+++ parts/nixos.nix
@@ -12,7 +12,7 @@ in
       inputs.sops-nix.nixosModules.sops
       inputs.lanzaboote.nixosModules.lanzaboote
       inputs.nix-flatpak.nixosModules.nix-flatpak
-      # inputs.hermes-agent.nixosModules.default
+      inputs.hermes-agent.nixosModules.default
       inputs.stylix.nixosModules.stylix
       {
         home-manager = {
