74dfaf7d079e0d8c3c5950d3562f721269959187
Lucas Cirille <lucas.cirille@gmail.com>
2026-09-08 20:40:00 -0300

    Auto-commit: 2026-09-08 20:40:00

--- home/home.nix
+++ home/home.nix
@@ -177,6 +177,12 @@ in
         dispatch_in_gateway = true;
         dispatch_interval_seconds = 10;
       };
+      browser = {
+        backend = "local";
+        # Tell the local backend to launch Chromium with debugging enabled
+        headless = false; # Set to true if you don't want the browser window to physically pop up on your screen
+        cdp_url = "http://127.0.0.1:9222";
+      };
       extraPackages = with pkgs; [
         uv        # Allows Hermes to run the 'browser-use' CLI on the fly via uvx
         chromium  # The browser engine it will control (keeps it isolated from your personal browser)
