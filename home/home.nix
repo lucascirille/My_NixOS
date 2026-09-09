8e7c1d514677c2361ccd345910a63c25d80dd038
Lucas Cirille <lucas.cirille@gmail.com>
2026-09-09 11:03:30 -0300

    Auto-commit: 2026-09-09 11:03:30

--- home/home.nix
+++ home/home.nix
@@ -22,7 +22,21 @@ in
     EDITOR = "nvim";
     # Tells 'nh' where your flake lives so you don't need to pass paths manually
     NH_FLAKE = "${config.home.homeDirectory}/.dotfiles";
-    # SUDO_ASKPASS = "${pkgs.gnome.gnome-keyring}/bin/gnome-keyring-askpass";
+    SUDO_ASKPASS = "${config.home.homeDirectory}/.local/bin/nixos-askpass";
+  };
+    # custom askpass script using your existing tools (rofi + libnotify)
+  home.file.".local/bin/nixos-askpass" = {
+    text = ''
+      #!/usr/bin/env bash
+      
+      # 1. Send a critical notification to dunst exactly when the password is requested
+      ${pkgs.libnotify}/bin/notify-send "NixOS Build" "🔐 Build finished! Password required to activate." -u critical -t 15000
+      
+      # 2. Prompt for password using rofi (which you already use). 
+      # -dmenu makes it read from stdin/stdout, -password hides the characters
+      ${pkgs.rofi}/bin/rofi -dmenu -password -p "🔐 Sudo Password"
+    '';
+    executable = true;
   };
   home.packages = with pkgs; [
 
