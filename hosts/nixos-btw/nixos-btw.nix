1d1eabb0acda45826ab7d38c924ff00def892516
Lucas Cirille <lucas.cirille@gmail.com>
2026-09-08 21:20:01 -0300

    Auto-commit: 2026-09-08 21:20:01

--- hosts/nixos-btw/nixos-btw.nix
+++ hosts/nixos-btw/nixos-btw.nix
@@ -283,13 +283,6 @@
     ];
   };
 
-  system.activationScripts.binbash = {
-    text = ''
-      mkdir -p /bin
-      ln -sf ${pkgs.bash}/bin/bash /bin/bash
-    '';
-    deps = [];
-  };
   system.userActivationScripts.steamDevCfg.text = ''
   mkdir -p "$HOME/.local/share/Steam"
   echo "unShaderBackgroundProcessingThreads $(nproc)" > "$HOME/.local/share/Steam/steam_dev.cfg"
