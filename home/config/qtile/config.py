d6bf11dcf89304a059550b1d5fe13b1d7b088a21
Lucas Cirille <lucas.cirille@gmail.com>
2026-09-13 23:11:42 -0300

    Auto-commit: 2026-09-13 23:11:42

--- home/config/qtile/config.py
+++ home/config/qtile/config.py
@@ -422,6 +422,8 @@ floating_layout = layout.Floating(
         Match(wm_class="makebranch"),
         Match(wm_class="maketag"),
         Match(wm_class="ssh-askpass"),
+        Match(wm_class="linux-wallpaperengine"),
+        Match(title="linux-wallpaperengine"),
         Match(title="branchdialog"),
         Match(title="pinentry"),
     ],
