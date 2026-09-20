7102110c8bbd2bec3fefbfb8917378c245815120
Lucas Cirille <lucas.cirille@gmail.com>
2026-09-20 01:30:26 -0300

    Auto-commit: 2026-09-20 01:30:26

--- hosts/nixos-btw/nixos-btw.nix
+++ hosts/nixos-btw/nixos-btw.nix
@@ -16,8 +16,6 @@
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 # --- TEMPORARY FIX: Gag libvirtd to break the crash loop ---
   virtualisation.libvirtd.enable = lib.mkForce false;
-  systemd.services.libvirtd.enable = lib.mkForce false;
-  systemd.services.libvirtd.wantedBy = lib.mkForce [ ];
 
   # VM / Baremetal Overrides
   specialisation.baremetal.configuration = {
