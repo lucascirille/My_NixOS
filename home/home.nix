be4d174c26f4bfe8971ec1cfc9cc9e4ad1d376f1
Lucas Cirille <lucas.cirille@gmail.com>
2026-09-22 23:47:42 -0300

    Auto-commit: 2026-09-22 23:47:42

--- home/home.nix
+++ home/home.nix
@@ -982,8 +982,17 @@ programs.brave = {
     #     destination = "/etc/chromium/native-messaging-hosts/org.keepassxc.keepassxc_browser.json";
     #   })
     # ];
-    nativeMessagingHosts = [
-    pkgs.keepassxc
+# Manually build the Brave directory because pkgs.keepassxc only has Chromium/Firefox
+  nativeMessagingHosts = [
+    (pkgs.writeTextDir "share/brave/native-messaging-hosts/org.keepassxc.keepassxc_browser.json" (builtins.toJSON {
+      name = "org.keepassxc.keepassxc_browser";
+      description = "KeePassXC integration with native messaging support";
+      path = "${pkgs.keepassxc}/bin/keepassxc-proxy";
+      type = "stdio";
+      allowed_origins = [
+        "chrome-extension://oboonakemofpalcgghocfoadofidjkkk/"
+      ];
+    }))
   ];
   };
 
