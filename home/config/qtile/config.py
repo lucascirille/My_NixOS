715184b710de69773103a137fac3ab381619f538
Lucas Cirille <lucas.cirille@gmail.com>
2026-09-25 14:08:45 -0300

    Auto-commit: 2026-09-25 14:08:45

--- home/config/qtile/config.py
+++ home/config/qtile/config.py
@@ -92,6 +92,7 @@ ICON_CATEGORIES = {
     "": ["thunar", "nautilus", "dolphin", "files"],
     "": ["discord", "vesktop", "slack", "teams"],
     "󰓇": ["spotify", "music", "ncmpcpp"],
+    "󰓓": ["steam"],
 }
 DEFAULT_ICON = ""
 _app_cache: dict[str, str] = {}
